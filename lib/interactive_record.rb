05507d64b66420f5d0a71fc9931d6d3f9a468d85
E-Vargo <vargoelliott@gmail.com>
2022-01-20 12:27:44 -0800

    commit tree 13c6aa7eb72fd3c56a5b54c2aac7c3f3b7b96c2b

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -3,4 +3,23 @@ require 'active_support/inflector'
 
 class InteractiveRecord
   
+    def initialize
+
+    end
+
+    def self.table_name
+
+    end
+
+    def self.column_names
+
+    end
+
+    def self.find_by_name
+
+    end
+
+    def self.find_by 
+
+    end
 end
